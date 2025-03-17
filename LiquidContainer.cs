7bec516beffcec5d032b17c16862e06683fa2746
Tadeusz Kucza-Kuczynski <tkuczakuczynski22@gmail.com>
2025-03-17 19:52:49 +0100

    Dodawanie plikow zadanie 2

---
+++ LiquidContainer.cs
@@ -0,0 +1,20 @@
+﻿namespace Task_02;
+
+public class LiquidContainer : Container
+{
+    public bool IsHazardous { get; }
+    
+    public LiquidContainer(double maxLoad, bool isHazardous, double height, double depth, double tareWeight) 
+        : base("L", maxLoad, 0, height, depth, tareWeight)
+    {
+        IsHazardous = isHazardous;
+    }
+    
+    public override void Load(double weight)
+    {
+        double maxAllowed = IsHazardous ? MaxLoad * 0.5 : MaxLoad * 0.9;
+        if (CurrentLoad + weight > maxAllowed)
+            throw new OverfillException($"Przeładowanie kontenera :  {SerialNumber}!");
+        base.Load(weight);
+    }
+}
---
+++ Ship.cs
@@ -0,0 +1,60 @@
+﻿namespace Task_02;
+
+public class Ship
+{
+    public string Name { get; }
+    public int MaxContainers { get; }
+    public double MaxWeight { get; }
+    public double MaxSpeed { get; }
+    public List<Container> Containers { get; } = new();
+
+    public Ship(string name, int maxContainers, double maxWeight)
+    {
+        Name = name;
+        MaxContainers = maxContainers;
+        MaxWeight = maxWeight;
+    }
+
+    public void LoadContainer(Container container)
+    {
+        if (Containers.Count >= MaxContainers)
+            throw new Exception("Za dużo kontenerów na statku!");
+        if (Containers.Sum(c => c.CurrentLoad) + container.CurrentLoad > MaxWeight)
+            throw new Exception("Przekroczono maksymalną wagę statku!");
+        Containers.Add(container);
+    }
+
+    public void UnloadContainer(string serialNumber)
+    {
+        Containers.RemoveAll(c => c.SerialNumber == serialNumber);
+    }
+
+    public void ReplaceContainer(string serialNumber, Container newContainer)
+    {
+        var index = Containers.FindIndex(c => c.SerialNumber == serialNumber);
+        if (index == -1)
+            throw new Exception("Nie znaleziono kontenera do zastąpienia!");
+        Containers[index] = newContainer;
+    }
+
+    public static void TransferContainer(Ship from, Ship to, string serialNumber)
+    {
+        var container = from.Containers.FirstOrDefault(c => c.SerialNumber == serialNumber);
+        if (container == null)
+            throw new Exception("Kontener nie istnieje na tym statku");
+        from.Containers.Remove(container);
+        to.LoadContainer(container);
+    }
+    
+    public void PrintInfo()
+    {
+        Console.WriteLine($"Nazwa Statku : {Name.ToUpper()} \n" +
+                          $"Maksymalna liczba kontenerów : {MaxContainers}\n" +
+                          $"Maksymalna waga wszystkich kontenerów : {MaxWeight}\n" +
+                          $"Maksymalna prędkówść : {MaxSpeed} węzłów \n");
+        foreach (var c in Containers)
+        {
+            Console.WriteLine(c);
+        }
+    }
+}
